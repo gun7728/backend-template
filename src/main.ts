7d59a9a8956a00a90c902712cdc8cff5b09a0bbb
gun7728 <gun7728@gmail.com>
2025-03-07 14:59:29 +0900

    template update

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-  })
+  });
   const document = await NestiaSwaggerComposer.document(app, {
     openapi: '3.1',
     servers: [
